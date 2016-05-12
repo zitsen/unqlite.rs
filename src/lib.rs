842e16e02bd0ef46323e9ae9d1a97ec00e72f10d
Huo Linhe <huolinhe@berrygenomics.com>
2016-05-12 17:51:52 +0800

    doc(crate): add crate document

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,72 @@
-#![feature(convert)]
+//! A high-level UnQlite database engine wrapper.
+//!
+//! NOTE: Some of the documents is stolen from [UnQlite Offical Website][unqlite].
+//!
+//! # What is UnQlite?
+//!
+//! >
+//! UnQLite is a software library which implements a *self-contained*, *serverless*,
+//! zero-configuration, transactional NoSQL database engine. UnQLite is a document store database
+//! similar to [MongoDB], [Redis], [CouchDB] etc. as well a standard Key/Value store similar to
+//! [BerkeleyDB], [LevelDB], etc.
+//! >
+//! UnQLite is an embedded NoSQL (Key/Value store and Document-store) database engine. Unlike most
+//! other NoSQL databases, UnQLite does not have a separate server process. UnQLite reads and
+//! writes directly to ordinary disk files. A complete database with multiple collections, is
+//! contained in **a single disk file**. The database file format is cross-platform, you can freely
+//! copy a database between 32-bit and 64-bit systems or between big-endian and little-endian
+//! architectures.
+//!
+//! # Port to Rust
+//!
+//! This crate is high-level UnQlite database wrapper for Rust. A low-level bindings wrapper
+//! is avaliable as a seperated crate: [unqlite-sys](https://crates.io/crates/unqlite-sys).
+//!
+//! # Usage
+//!
+//! You can start with `UnQlite` constructors:
+//!
+//! ```
+//! extern crate unqlite;
+//!
+//! use unqlite::{UnQlite, Config, KV, Cursor};
+//!
+//! # #[cfg(feature = "enable-threads")]
+//! fn main() {
+//!     // The database memory is not handled by Rust, and the database is in-disk,
+//!     // so `mut` is not neccessary.
+//!     let unqlite = UnQlite::create_temp();
+//!     // Use any type that can use as `[u8]`
+//!     unqlite.kv_store("key", "a long length value").unwrap();
+//!     unqlite.kv_store("abc", [1,2,3]).unwrap();
+//!
+//!     let mut entry = unqlite.first();
+//!     // Iterate records
+//!     loop {
+//!         if entry.is_none() { break; }
+//!
+//!         let record = entry.expect("valid entry");
+//!         let (key, value) = record.key_value();
+//!         println!("* Go through {:?} --> {:?}", key, value);
+//!
+//!         if value.len() > 10 {
+//!             println!("** Delete key {:?} by value length", key);
+//!             entry = record.delete();
+//!         } else {
+//!             entry = record.next();
+//!         }
+//!     }
+//!     //panic!("for test");
+//! }
+//! # #[cfg(not(feature = "enable-threads"))]
+//! # fn main() { }
+//! ```
+//!
+//! [unqlite]: https://unqlite.org/index.html
+
+#![feature(shared)]
 #![feature(unique)]
+#![feature(concat_idents)]
 
 extern crate unqlite_sys as ffi;
 extern crate libc;
@@ -7,25 +74,42 @@ extern crate libc;
 #[cfg(test)]
 extern crate tempfile;
 
-pub use error::*;
-
-#[macro_use]
-#[allow(dead_code, non_camel_case_types)]mod error;
-
-use std::mem;
 use std::ffi::CString;
-use ffi::{unqlite_close, unqlite_open};
-use std::ptr::Unique;
+use std::mem;
+use std::ptr::Shared;
 
-/// UnQlite database entity.
+use ffi::{unqlite_close, unqlite_open};
+
+use error::Wrap;
+pub use error::{Error, Result};
+
 pub struct UnQlite {
-    db: Unique<::ffi::unqlite>,
+    engine: Shared<::ffi::unqlite>,
 }
 
-unsafe impl Send for UnQlite {}
-unsafe impl Sync for UnQlite {}
+macro_rules! eval {
+    ($i: ident, $($e: expr),*) => (
+        unsafe {
+            concat_idents!(unqlite_, $i)($($e),*)
+        }
+    );
+}
 
-impl<'open> UnQlite {
+macro_rules! wrap {
+    ($i: ident, $($e: expr),*) => (eval!($i, $($e),*).wrap());
+}
+
+
+macro_rules! wrap_raw {
+    ($self_:ident, $i: ident) => (
+        wrap!($i, $self_.as_raw_mut_ptr())
+    );
+    ($self_:ident, $i: ident, $($e: expr),+) => (
+        wrap!($i, $self_.as_raw_mut_ptr(), $($e),+)
+    );
+}
+
+impl UnQlite {
     /// Create UnQlite database at specific path.
     ///
     /// ```ignore
@@ -33,14 +117,12 @@ impl<'open> UnQlite {
     /// let _ = UnQlite::open(String::new());
     /// ```
     #[inline]
-    fn open<P: AsRef<str>>(filename: P, mode: OpenMode) -> ::Result<UnQlite> {
-        unsafe {
-            let mut db: *mut ::ffi::unqlite = mem::uninitialized();
-            let filename = filename.as_ref();
-            let filename = try!(CString::new(filename));
-            error_or!(unqlite_open(&mut db, filename.as_ptr(), mode.into()),
-                      UnQlite { db: Unique::new(db) })
-        }
+    fn open<P: AsRef<str>>(filename: P, mode: OpenMode) -> Result<UnQlite> {
+        let mut db: *mut ::ffi::unqlite = unsafe { mem::uninitialized() };
+        let filename = filename.as_ref();
+        let filename = try!(CString::new(filename));
+        wrap!(open, &mut db, filename.as_ptr(), mode.into())
+            .map(|_| UnQlite { engine: unsafe { Shared::new(db) } })
     }
 
     /// Create UnQlite database as `filename`.
@@ -139,14 +221,17 @@ impl<'open> UnQlite {
         Self::open(filename, OpenMode::ReadOnly).unwrap()
     }
 
-    fn close(&mut self) -> ::Result<()> {
-        unsafe { error_or!(unqlite_close(self.as_raw_mut_ptr())) }
+    fn close(&self) -> Result<()> {
+        wrap!(close, self.as_raw_mut_ptr())
     }
 
     unsafe fn as_raw_mut_ptr(&self) -> *mut ::ffi::unqlite {
-        *self.db
+        *self.engine
     }
 }
+
+unsafe impl Send for UnQlite {}
+unsafe impl Sync for UnQlite {}
 
 impl Drop for UnQlite {
     fn drop(&mut self) {
@@ -154,6 +239,7 @@ impl Drop for UnQlite {
     }
 }
 
+mod error;
 mod openmode;
 mod config;
 mod util;
@@ -162,7 +248,10 @@ mod kv_store;
 mod kv_cursor;
 
 pub use self::openmode::OpenMode;
-pub use self::util::Mmap;
+pub use self::config::Config;
+pub use self::transaction::Transaction;
+pub use self::util::*;
+pub use self::kv_store::*;
 pub use self::kv_cursor::*;
 
 #[cfg(test)]
